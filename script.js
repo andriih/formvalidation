d2b395e4fde4797366b0dcad1066d8d177f6055b
Your Name <you@example.com>
2017-01-23 20:47:29 +0200

    second commit

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
 var form = document.forms.myForm,
-	fname = document.getElementById("fname"),
-		fname__err = document.getElementById("fname__err"),
-	lname = document.getElementById("lname"),
- 	prog = document.getElementById("prog"),
- 	lang = document.getElementById("lang__hidden");
- 	lang__err = document.getElementById("lang__err");
+    fname = form.fname,
+    mail = form.mail,
+    login = form.sbmt,
+    fname__err = form.fname__err,
+    pass__err = form.pass__err,
+    log_nav = document.querySelector(".login"),
+    reg_nav = document.querySelector(".reg"),
 
+    form2 = document.forms.myForm2;
 
-form.addEventListener("onsubmit",function(e){
-	e.preventDefault();
-
+log_nav.addEventListener("click",function(){
+    form.style.display = "block";
+});
+reg_nav.addEventListener("click",function(){
+    form2.style.display = "block";
+    form.style.display = "none";
 });
 
 fname.addEventListener("keyup",function(){
-	if(this.value.length <= 3){
-		fname__err.hidden = false;
-		fname__err.innerHTML = "Name Shoud be longer then 3 chars";
-	}else{
-
-		fname__err.hidden = true;
-	}
-});
-lname.addEventListener("keyup",function(){
-	if(this.value.length <= 3){
-		lname__err.hidden = false;
-		lname__err.innerHTML = "Last name Shoud be<br /> longer then 3 chars";
-	}else{
-
-		lname__err.hidden = true;
-	}
-});
-
-prog.addEventListener("change",function(){
-	 if(this.value === "y"){
-	 	lang.hidden = false;
-	 	var lang__select  =  document.getElementById("lang");
-	 	console.log(lang__select.value);
-	 	lang__select.addEventListener("change",function(){
-	 		if(this.value === "select"){
-	 			lang__err.innerHTM = "You should choose programming language";
-	 		}else{
-
-	 		}
-	 	});
-	}else{
-		lang.hidden = true;
-	}
-	console.log(this.value);
+    this.value = fname.value.trim();
+    if(this.value.length <= 3){
+        fname__err.innerHTML= "errr";
+    }
 });
