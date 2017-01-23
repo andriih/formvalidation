855d3aa1a62046fd8452ecb91ae58cfee1cb0775
Your Name <you@example.com>
2017-01-23 21:09:03 +0200

    innit3

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 var form = document.forms.myForm,
-    fname = form.fname,
+    fname = form.mail,
     mail = form.mail,
     login = form.sbmt,
-    fname__err = form.fname__err,
+    mail__err = form.mail__err,
+	pass = form.pass;
     pass__err = form.pass__err,
     log_nav = document.querySelector(".login"),
     reg_nav = document.querySelector(".reg"),
 
     form2 = document.forms.myForm2;
 
+
 log_nav.addEventListener("click",function(){
     form.style.display = "block";
+    form2.style.display = "none";
 });
 reg_nav.addEventListener("click",function(){
     form2.style.display = "block";
     form.style.display = "none";
 });
 
-fname.addEventListener("keyup",function(){
-    this.value = fname.value.trim();
-    if(this.value.length <= 3){
-        fname__err.innerHTML= "errr";
+form.addEventListener("submit",function(e){
+   e.preventDefault();
+
+    this.valueMail = mail.value.trim();
+    this.valuePass = pass.value.trim();
+
+
+
+    if(this.valueMail.length <= 3){
+    	mail__err.innerHTML = "E";
     }
 });
